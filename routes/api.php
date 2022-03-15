b93177cfb7c67ce0f6119429129a89ab78f0801d
excepweb <80252689+excepweb@users.noreply.github.com>
2022-03-15 20:13:46 +0100

    rutes de api a mig fer, falta el controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,31 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+POST /players : crea un jugador
+PUT /players/{id} : modifica el nom del jugador
+POST /players/{id}/games/ : un jugador específic realitza una tirada dels daus.
+DELETE /players/{id}/games: elimina les tirades del jugador
+GET /players: retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits 
+GET /players/{id}/games: retorna el llistat de jugades per un jugador.
+GET /players/ranking: retorna el ranking mig de tots els jugadors del sistema. 
+    És a dir, el percentatge mig d’èxits.
+GET /players/ranking/loser: retorna el jugador amb pitjor percentatge d’èxit
+GET /players/ranking/winner: retorna el jugador amb pitjor percentatge d’èxit.
+*/
+
+Route::post('/players', [APIPlayerController::class, 'store']);
+Route::put('/players/{id}', [APIPlayerController::class, 'update']);
+
+Route::post('/players/{id}/games', [APIGameController::class, 'store']);
+Route::delete('/players/{id}/games', [APIGameController::class, 'delete']);
+
+Route::get('/players', [APIPlayerController::class, 'index']);
+Route::get('/players/{id}/games', [APIGameController::class, 'show']);
+
+Route::get('/players/ranking', [APIGameController::class, 'ranking']);
+Route::get('/players/ranking/loser', [APIGameController::class, 'ranking_loser']);
+Route::get('/players/ranking/winner', [APIGameController::class, 'ranking_winner']);
+
+
